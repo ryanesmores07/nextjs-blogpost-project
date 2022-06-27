890c196999c1ba99332d17f594fbdfa8e72afef8
Ryan Esmores <ryanesmores07@gmail.com>
2022-06-27 20:51:10 +0900

    fixing layout bug

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
-import Layout from "../components/layout/Layout";
+
+import Layout from "../components/Layout/Layout";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
